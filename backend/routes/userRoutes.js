b1146de03e18be8fbbcc41b704fe87cbce49036a
Abdul-Muneeb-Qureshi <115551959+Abdul-Muneeb-Qureshi@users.noreply.github.com>
2025-04-04 16:32:31 +0500

    first commit

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+// Routes for CRUD operations on users
+router.post('/api/users', userController.addUser);
+router.put('/api/users', userController.updateUser);
+router.delete('/api/users', userController.deleteUser);
+router.post('/api/login', userController.login);
+router.get('/api/users', userController.getAllUsers);
+router.get('/api/notifications/:email', userController.getNotificationsByEmail);
+
+router.put('/api/notifications', userController.markNotificationsAsRead);
+
+
+
+
+module.exports = router;
